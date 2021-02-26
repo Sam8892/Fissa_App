8d3d55227aa3d38b51e5a3633d2fdc28465adf19
Stage2021 <stage2021@imac-de-stagiaireios.local>
2021-02-26 11:18:02 +0100

    Initial Commit

---
+++ Fissa_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fissa_App
+//
+//  Created by Stage2021 on 26/02/2021.
+//  Copyright © 2021 Stage2021. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Fissa_AppTests/Fissa_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fissa_AppTests.swift
+//  Fissa_AppTests
+//
+//  Created by Stage2021 on 26/02/2021.
+//  Copyright © 2021 Stage2021. All rights reserved.
+//
+
+import XCTest
+@testable import Fissa_App
+
+class Fissa_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
